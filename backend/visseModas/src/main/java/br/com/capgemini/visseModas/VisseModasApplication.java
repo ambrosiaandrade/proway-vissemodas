bc49c6c783f8cc27dc6605b122db31707b78d6a6
valquiria matter <matter.valquiria@gmail.com>
2021-11-05 14:41:28 -0300

    projeto inicial

---
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/VisseModasApplication.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.visseModas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisseModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisseModasApplication.class, args);
+	}
+
+}
