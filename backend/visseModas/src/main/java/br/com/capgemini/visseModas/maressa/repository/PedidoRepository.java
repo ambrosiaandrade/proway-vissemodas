811d195ebff3fa744983852365a3e4c85ed54192
maressa.oliveira25 <maressavargasoliveira@gmail.com>
2021-11-08 14:29:17 -0300

    PedidoRepository.java

--- backend/visseModas/src/main/java/br/com/capgemini/visseModas/maressa/repository/PedidoRepository.java
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/maressa/repository/PedidoRepository.java
@@ -1,10 +1,16 @@
 package br.com.capgemini.visseModas.maressa.repository;
 
 import br.com.capgemini.visseModas.maressa.model.Pedido;
+import br.com.capgemini.visseModas.maressa.model.Situacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
-    
+    //@Query("select p from pedido p where p.situacao = :situacao")
+    //List<Pedido> findBySituacao(@Param("situacao") Situacao situacao);
 }
