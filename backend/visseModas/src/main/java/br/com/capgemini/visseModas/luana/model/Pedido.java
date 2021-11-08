627ab3fb94164e3afc9e91dc3787dfafe90de2ee
Luana Bezerra <lu.oliveira1994@hotmail.com>
2021-11-07 21:09:16 -0300

    atualizacao meu projeto

---
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/luana/model/Pedido.java
@@ -0,0 +1,93 @@
+package br.com.capgemini.visseModas.luana.model;
+
+import java.time.LocalDate;
+
+public class Pedido {
+
+    private Integer id;
+    private LocalDate data;
+    private Cliente cliente;
+    private Endereco endereco;
+    private Situacao situacao;
+    private Double valorTotal;
+    private Integer quantidadeTotal;
+    private Double percentualDesconto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Situacao getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Situacao situacao) {
+        this.situacao = situacao;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Integer getQuantidadeTotal() {
+        return quantidadeTotal;
+    }
+
+    public void setQuantidadeTotal(Integer quantidadeTotal) {
+        this.quantidadeTotal = quantidadeTotal;
+    }
+
+    public Double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(Double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", data=" + data +
+                ", cliente=" + cliente +
+                ", endereco=" + endereco +
+                ", situacao=" + situacao +
+                ", valorTotal=" + valorTotal +
+                ", quantidadeTotal=" + quantidadeTotal +
+                ", percentualDesconto=" + percentualDesconto +
+                '}';
+    }
+}
