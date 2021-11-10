d5b7a17e2905b8e5f7b959b52de5293c488b8286
maressa.oliveira25 <maressavargasoliveira@gmail.com>
2021-11-10 11:34:31 -0300

    Criando ItemPedidoService

---
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/controller/service/ItemPedidoService.java
@@ -0,0 +1,57 @@
+package br.com.capgemini.visseModas.controller.service;
+
+import br.com.capgemini.visseModas.model.dto.ItemPedidoDTO;
+import br.com.capgemini.visseModas.model.entity.ItemPedido;
+import br.com.capgemini.visseModas.model.repository.ItemPedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemPedidoService {
+
+    @Autowired
+    private ItemPedidoRepository repository;
+
+    public void salvar(ItemPedido itemPedido){
+        repository.save(itemPedido);
+    }
+
+    private void deletar(Long id){
+        repository.deleteById(id);
+    }
+    public List<ItemPedido> listarTudo(){
+        return repository.findAll();
+    }
+
+    // find com DTO
+    public List<ItemPedidoDTO> listarTudoDTO(){
+        List<ItemPedido> listaItemPedido = repository.findAll();
+        return ItemPedidoDTO.converter(listaItemPedido);
+    }
+
+    public ItemPedido alterar(Long id){
+
+        // recebe do banco de dados
+        Optional<ItemPedido> itemPedidoBuscada = repository.findById(id);
+
+        //verificando se existe
+        if(!itemPedidoBuscada.isPresent()){
+            return null;
+        }
+        // converte o Optional
+        ItemPedido itemPedido = itemPedidoBuscada.get();
+
+        // criando novo objeto
+        ItemPedido itemPedidoNovo = new ItemPedido();
+
+        // setando
+        itemPedidoNovo.setProduto(itemPedido.getProduto());
+        itemPedidoNovo.setQuantidade(itemPedido.getQuantidade());
+        itemPedidoNovo.setValorTotal(itemPedido.getValorTotal());
+        repository.save(itemPedido);
+        return itemPedido;
+    }
+}
