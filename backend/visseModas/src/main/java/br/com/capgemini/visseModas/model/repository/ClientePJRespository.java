11cb9eb5e7da352a14e70d035d0381d98bbd215d
valquiria matter <matter.valquiria@gmail.com>
2021-11-10 07:28:37 -0300

    Refatorando e Add Properties

---
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/model/repository/ClientePJRespository.java
@@ -0,0 +1,7 @@
+package br.com.capgemini.visseModas.model.repository;
+
+import br.com.capgemini.visseModas.model.entity.ClientePJ;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientePJRespository extends JpaRepository<ClientePJ, Long> {
+}
