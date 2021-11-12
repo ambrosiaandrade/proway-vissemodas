c0e16f9e8325a1c6d10cce23a6a1b7acfc6b50ff
maressa.oliveira25 <maressavargasoliveira@gmail.com>
2021-11-12 15:49:12 -0300

    DTO Saída com erro ainda

--- backend/visseModas/src/main/java/br/com/capgemini/visseModas/model/dtoSaida/ItemPedidoDTOSaida.java
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/model/dtoSaida/ItemPedidoDTOSaida.java
@@ -8,19 +8,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-public class ItemPedidoDTO {
+public class ItemPedidoDTOSaida {
+
+    private Long id;
     private Produto produto;
     private Integer quantidade;
     private Double valorTotal;
 
-    public ItemPedidoDTO(ItemPedido itemPedido){
+    public ItemPedidoDTOSaida(){
+
+    }
+
+    public ItemPedidoDTOSaida(ItemPedido itemPedido){
+        this.id = itemPedido.getId();
         this.produto = itemPedido.getProduto();
         this.quantidade = itemPedido.getQuantidade();
         this.valorTotal = itemPedido.getValorTotal();
     }
 
-    public static List<ItemPedidoDTO> converter (List<ItemPedido> listaItemPedido){
-        return listaItemPedido.stream().map(ItemPedidoDTO::new).collect(Collectors.toList());
+    public static List<ItemPedidoDTOSaida> converter (List<ItemPedido> listaItemPedido){
+        return listaItemPedido.stream().map(ItemPedidoDTOSaida::new).collect(Collectors.toList());
     }
 
+    /*public static ItemPedido dtoToItemPedido() {
+        //estudar mais um pouco como fazer essa parte
+        ItemPedido itemPedido = new ItemPedido();
+
+        itemPedido.setId(id);
+        itemPedido.setProduto(produto);
+        itemPedido.setQuantidade(quantidade);
+        itemPedido.setValorTotal(valorTotal);
+
+        return itemPedido;
+    }*/
 }
