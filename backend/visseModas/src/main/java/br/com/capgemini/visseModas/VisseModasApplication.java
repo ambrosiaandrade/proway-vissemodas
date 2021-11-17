54baf8d9301c114edec8859fd0f77dce0a887881
valquiria matter <matter.valquiria@gmail.com>
2021-11-17 09:03:08 -0300

    add swagger

--- backend/visseModas/src/main/java/br/com/capgemini/visseModas/VisseModasApplication.java
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/VisseModasApplication.java
@@ -4,9 +4,11 @@ package br.com.capgemini.visseModas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport //dar suporte a paginacao
+@EnableSwagger2
 public class VisseModasApplication {
 
 	public static void main(String[] args) {
