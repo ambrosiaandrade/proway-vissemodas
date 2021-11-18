2d4a40ef727a4dc3e905d4849aace4fc6da57df4
RebecaKarinne <rebeca.karinne@gmail.com>
2021-11-18 14:03:52 -0300

    Início cliente add e list

--- frontend/vissemodas/src/app/app.module.ts
+++ frontend/vissemodas/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { HomeComponent } from './views/home/home.component';
 import { NavbarComponent } from './views/shared/navbar/navbar.component';
 import { FooterComponent } from './views/shared/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { ClienteComponent } from './views/cliente/cliente.component';
+import { AddClienteComponent } from './cliente/add-cliente/add-cliente.component';
+import { ListClienteComponent } from './views/cliente/list-cliente/list-cliente.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +23,18 @@ import { ReactiveFormsModule } from '@angular/forms';
     ListProdutoComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ClienteComponent,
+    AddClienteComponent,
+    ListClienteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
