0568902f23fa8a240028bdf9fb0d1263ffc30ae0
Amanda Medeiros <medeiros.amandas@gmail.com>
2021-11-19 08:54:32 -0300

    adicionando nas rotas o add-endereco

--- frontend/vissemodas/src/app/app-routing.module.ts
+++ frontend/vissemodas/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEnderecoComponent } from './views/endereco/add-endereco/add-endereco.component';
 import { HomeComponent } from './views/home/home.component';
 import { AddProdutoComponent } from './views/produto/add-produto/add-produto.component';
 import { ListProdutoComponent } from './views/produto/list-produto/list-produto.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'add-produto', component: AddProdutoComponent},
   {path: 'list-produto', component: ListProdutoComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+  {path: 'add-endereco', component: AddEnderecoComponent}
 ];
 
 @NgModule({
