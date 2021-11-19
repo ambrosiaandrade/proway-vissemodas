37ead9324e8678606fe74812cc9593f0f4776023
Amanda Medeiros <medeiros.amandas@gmail.com>
2021-11-19 11:18:33 -0300

    fazendo viacep funcionar e mostrar no form

--- frontend/vissemodas/src/app/views/endereco/add-endereco/add-endereco.component.ts
+++ frontend/vissemodas/src/app/views/endereco/add-endereco/add-endereco.component.ts
@@ -1,4 +1,7 @@
+import { BinaryOperator } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConsultaCepService } from 'src/app/services/consulta-cep.service';
 
 @Component({
   selector: 'app-add-endereco',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-endereco.component.css']
 })
 export class AddEnderecoComponent implements OnInit {
-
-  constructor() { }
+  
+  enderecoForm: FormGroup
+  constructor(private _fb: FormBuilder, private CepService: ConsultaCepService) {
+    this.enderecoForm = _fb.group({
+      cep: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      cidade: ['', Validators.required],
+      estado: ['', Validators.required],
+      bairro: ['', Validators.required],
+      numero: ['', Validators.required]
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  populaFormEndereco(dados: any){
+    console.log(dados)
+    this.enderecoForm.patchValue({
+      logradouro: dados.logradouro,
+      cidade: dados.localidade,
+      estado: dados.uf,
+      bairro: dados.bairro
+    })
+  }
+  consultaCEP(){
+    let cep = this.enderecoForm.get('cep')?.value;
+    if(cep != null && cep !== ''){
+      this.CepService.consultaCEP(cep)?.subscribe(dados => this.populaFormEndereco(dados))
+      
+    }
+  }
+
+
+
+
+  addEndereco(){
+    console.log(this.enderecoForm.value)
+    console.log(this.enderecoForm.get('cep')?.value)
   }
 
 }
