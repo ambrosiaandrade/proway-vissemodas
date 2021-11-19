e9163f18c1db0d66b44a3db4fe7a0c81126fc9ad
Amanda Medeiros <medeiros.amandas@gmail.com>
2021-11-19 14:45:30 -0300

    salvando endereco no banco

--- frontend/vissemodas/src/app/views/endereco/add-endereco/add-endereco.component.ts
+++ frontend/vissemodas/src/app/views/endereco/add-endereco/add-endereco.component.ts
@@ -1,7 +1,9 @@
 import { BinaryOperator } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Endereco } from 'src/app/models/endereco.model';
 import { ConsultaCepService } from 'src/app/services/consulta-cep.service';
+import { EnderecoService } from 'src/app/services/endereco.service';
 
 @Component({
   selector: 'app-add-endereco',
@@ -11,7 +13,9 @@ import { ConsultaCepService } from 'src/app/services/consulta-cep.service';
 export class AddEnderecoComponent implements OnInit {
   
   enderecoForm: FormGroup
-  constructor(private _fb: FormBuilder, private CepService: ConsultaCepService) {
+
+  constructor(private _fb: FormBuilder, private CepService: ConsultaCepService, 
+   private _service: EnderecoService) {
     this.enderecoForm = _fb.group({
       cep: ['', Validators.required],
       logradouro: ['', Validators.required],
@@ -42,12 +46,23 @@ export class AddEnderecoComponent implements OnInit {
     }
   }
 
-
-
-
   addEndereco(){
     console.log(this.enderecoForm.value)
     console.log(this.enderecoForm.get('cep')?.value)
+    const ENDERECO: Endereco = {
+      cep: this.enderecoForm.get('cep')?.value,
+      logradouro: this.enderecoForm.get('logradouro')?.value,
+      cidade: this.enderecoForm.get('cidade')?.value,
+      estado: this.enderecoForm.get('estado')?.value,
+      bairro: this.enderecoForm.get('bairro')?.value,
+      numero: this.enderecoForm.get('numero')?.value,
+    }
+    this._service.postEndereco(ENDERECO).subscribe({
+      next: (data: any) => {
+        console.log("Endereço cadastrado")
+      },
+      error: (e) => console.log(e)
+    })
   }
 
 }
