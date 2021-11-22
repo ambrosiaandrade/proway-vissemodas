496dddb36d68b7bcc0cbc079c4e1aaa59c6294cb
ambrosiaandrade <70069811+ambrosiaandrade@users.noreply.github.com>
2021-11-22 11:02:42 -0300

    Novo produto

--- frontend/vissemodas/src/app/services/cliente.service.ts
+++ frontend/vissemodas/src/app/services/cliente.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Cliente } from '../models/cliente.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
-
   private readonly url = 'http://localhost:8080/clientes';
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
-  getClientes(): Observable<any>{
+  getClientes(): Observable<any> {
     return this._http.get(this.url);
   }
 
-  postCliente(cliente: Cliente): Observable<any>{
-    return this._http.post(this.url, cliente);
+  postCliente(cliente: Cliente): Observable<any> {
+    return this._http
+      .post(this.url, cliente)
+      .pipe(catchError(this.handleError));
   }
 
-  deleteCliente(id: any):Observable<any>{
+  handleError(error: any) {
+    // return throwError(error.message || 'Server error');
+    return throwError(error.error.errors[0].defaultMessage);
+  }
+
+  deleteCliente(id: any): Observable<any> {
     return this._http.delete(`${this.url}/${id}`);
   }
 }
