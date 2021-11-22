e3906b5435d4045936cba8ed9a35cc255a68f3ac
ambrosiaandrade <70069811+ambrosiaandrade@users.noreply.github.com>
2021-11-22 17:43:26 -0300

    LocalStorage

--- frontend/vissemodas/src/app/views/carrinho/carrinho.component.ts
+++ frontend/vissemodas/src/app/views/carrinho/carrinho.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Produto } from 'src/app/models/produto.model';
+import { ItemPedidoService } from 'src/app/services/item-pedido.service';
 import { ListProdutoComponent } from '../produto/list-produto/list-produto.component';
 
 @Component({
@@ -8,12 +10,36 @@ import { ListProdutoComponent } from '../produto/list-produto/list-produto.compo
   styleUrls: ['./carrinho.component.css'],
 })
 export class CarrinhoComponent implements OnInit {
-  constructor(private _router: Router) {
-  // let listarCarrinho = _produto.carrinho
+  // Vem do localStorage
+  listItensPedido: Produto[] = [];
 
+  constructor(private _router: Router) {
+    // let listarCarrinho = _produto.carrinho
   }
-  
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.buscarItemPedido();
+  }
+
+  buscarItemPedido() {
+    if (localStorage.getItem('list')) {
+      this.listItensPedido = JSON.parse(localStorage.getItem('list') || '{}');
+    } else {
+      this.listItensPedido = [];
+    }
+  }
+
+  removerItemPedido(id: any) {
+    // filter() retorna uma lista que satisfaz a condição,
+    // neste caso, ser diferente do id passado por parâmetro
+    // logo, retorna tudo exceto o que tiver esse id
+    this.listItensPedido = this.listItensPedido.filter(
+      (item) => item.id !== id
+    );
+
+    // Atribuindo essa nova lista filtrada para o localStorage
+    localStorage.setItem('list', JSON.stringify(this.listItensPedido));
+  }
 
   finalizarCompra() {
     // TODO: fazer a implementação
