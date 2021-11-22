9f4ecda81754189354a1661b3db8af8d77ed3f48
Luana Bezerra <lu.oliveira1994@hotmail.com>
2021-11-22 16:39:09 -0300

    add paginacao 8 no findAll de pedido, not blank e comentado codigo de calculo desconto no backend

--- backend/visseModas/src/main/java/br/com/capgemini/visseModas/services/PedidoService.java
+++ backend/visseModas/src/main/java/br/com/capgemini/visseModas/services/PedidoService.java
@@ -90,13 +90,13 @@ public class PedidoService {
 //    }
 
 
-    public BigDecimal calcularDesconto(Pedido pedido, BigDecimal percentualDesconto){
+    /*public BigDecimal calcularDesconto(Pedido pedido, BigDecimal percentualDesconto){
         if(pedido.getSituacao().equals("ABERTO")) {
             BigDecimal valorFinal = pedido.getValorTotal().multiply(percentualDesconto);
             return valorFinal;
         }
         return (new BigDecimal(0));
-    }
+    }*/
 
     public Pedido buscarPorId(Long id) {
         Optional<Pedido> optional = pedidoRepository.findById(id);
