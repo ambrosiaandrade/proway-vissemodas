953125fe4a8815fd974f1473e5df3c75b55df261
ambrosiaandrade <70069811+ambrosiaandrade@users.noreply.github.com>
2021-11-24 12:24:32 -0300

    Organizando as listas admin

--- frontend/vissemodas/src/app/app-routing.module.ts
+++ frontend/vissemodas/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddClienteComponent } from './views/cliente/add-cliente/add-cliente.component';
-import { ListClienteComponent } from './views/cliente/list-cliente/list-cliente.component';
+import { AdminListClienteComponent } from './views/cliente/admin-list-cliente/admin-list-cliente.component';
 import { CarrinhoComponent } from './views/carrinho/carrinho.component';
 import { AddEnderecoComponent } from './views/endereco/add-endereco/add-endereco.component';
 import { HomeComponent } from './views/home/home.component';
@@ -9,7 +9,7 @@ import { AddProdutoComponent } from './views/produto/add-produto/add-produto.com
 import { ListProdutoComponent } from './views/produto/list-produto/list-produto.component';
 import { FinalizadoComponent } from './views/finalizado/finalizado.component';
 import { AdminListProdutoComponent } from './views/produto/admin-list-produto/admin-list-produto.component';
-import { HistoryPedidoComponent } from './views/pedido/history-pedido/history-pedido.component';
+import { AdminListPedidoComponent } from './views/pedido/admin-list-pedido/admin-list-pedido.component';
 import { ChooseClienteComponent } from './views/cliente/choose-cliente/choose-cliente.component';
 
 const routes: Routes = [
@@ -19,14 +19,14 @@ const routes: Routes = [
   { path: 'list-produto', component: ListProdutoComponent },
   { path: 'admin-list-produto', component: AdminListProdutoComponent },
   { path: 'add-cliente', component: AddClienteComponent },
-  { path: 'list-cliente', component: ListClienteComponent },
+  { path: 'admin-list-cliente', component: AdminListClienteComponent },
   { path: 'edit-cliente/:id', component: AddClienteComponent }, // editar cliente (não sei se era necessário, mas fiz porque confundi com o edit endereço)
   { path: 'add-endereco', component: AddEnderecoComponent },
   { path: 'edit-endereco/:id', component: AddEnderecoComponent }, // editar endereço (esse era o correto para fazer)
   { path: 'choose-cliente', component: ChooseClienteComponent },
   { path: 'carrinho', component: CarrinhoComponent },
   { path: 'finalizado', component: FinalizadoComponent },
-  { path: 'history-pedido', component: HistoryPedidoComponent },
+  { path: 'admin-list-pedido', component: AdminListPedidoComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
