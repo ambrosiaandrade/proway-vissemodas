7d638baf4ce641e51d78472717d51503e5aff067
RebecaKarinne <rebeca.karinne@gmail.com>
2021-11-24 17:35:44 -0300

    Início botão buscar

--- frontend/vissemodas/src/app/views/home/home.component.ts
+++ frontend/vissemodas/src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from 'src/app/services/produto.service';
+import { Produto } from 'src/app/models/produto.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  listProdutos: Produto[] = [];
+
+  constructor(private _service: ProdutoService) {}
+
+  ngOnInit(): void {
+    this.buscarProduto();
+  } 
+
+  buscarProduto() {
+    this._service.getProdutos().subscribe({
+      next: (data) => {
+        this.listProdutos = data;
+        console.log(data);
+      },
+      error: (e) => console.log(e),
+    });
+  }
+
 }
