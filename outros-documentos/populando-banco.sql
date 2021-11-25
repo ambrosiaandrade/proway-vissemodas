901d45ddf95755b95256b13945de5de080016b01
Amanda Medeiros <medeiros.amandas@gmail.com>
2021-11-25 15:29:30 -0300

    incluindo duas bolsas

--- outros-documentos/populando-banco.sql
+++ outros-documentos/populando-banco.sql
@@ -3,9 +3,9 @@ create schema vissemodas;
 use vissemodas;
 
 insert into produto(descricao, tamanho, valor_unitario, categoria, imagem, status) values 
-('calÃ§a jogger preta', '38', 130.00, 'calÃ§as', 'assets/produtos/calca-1.png', 1),
-('calÃ§a alfaiataria xadrez', '36', 140.00, 'calÃ§as', 'assets/produtos/calca-2.png', 1),
-('calÃ§a jogger cinza', '42', 110.00, 'calÃ§as', 'assets/produtos/calca-3.png', 1);
+('calça jogger preta', '38', 130.00, 'calças', 'assets/produtos/calca-1.png', 1),
+('calça alfaiataria xadrez', '36', 140.00, 'calças', 'assets/produtos/calca-2.png', 1),
+('calça jogger cinza', '42', 110.00, 'calças', 'assets/produtos/calca-3.png', 1);
 
 insert into produto(descricao, tamanho, valor_unitario, categoria, imagem, status) values 
 ('camisa listrada azul', 'G', 130.00, 'camisas', 'assets/produtos/camisa-1.png', 1),
@@ -18,13 +18,19 @@ insert into produto(descricao, tamanho, valor_unitario, categoria, imagem, statu
 ('mochila tie-dye now united', 40, 140.00, 'mochilas', 'assets/produtos/mochila-2.png', 1),
 ('mochila preta naruto', 36, 110.00, 'mochilas', 'assets/produtos/mochila-3.png', 1),
 ('mochila colorida hamster', 36, 110.00, 'mochilas', 'assets/produtos/mochila-4.png', 1),
-('mochila roxa unicÃ³rnio', 36, 110.00, 'mochilas', 'assets/produtos/mochila-5.png', 1);
+('mochila roxa unicórnio', 36, 110.00, 'mochilas', 'assets/produtos/mochila-5.png', 1);
 
 insert into produto(descricao, tamanho, valor_unitario, categoria, imagem, status) values 
 ('sapato social preto', 38, 130.00, 'sapatos', 'assets/produtos/sapato-1.png', 1),
 ('mocassim verde musgo', 40, 140.00, 'sapatos', 'assets/produtos/sapato-2.png', 1),
 ('sapato social marrom', 36, 110.00, 'sapatos', 'assets/produtos/sapato-3.png', 1),
 ('sapato boneca nude', 36, 110.00, 'sapatos', 'assets/produtos/sapato-4.png', 1);
+
+INSERT INTO
+	produto(descricao,tamanho,valor_unitario,categoria,imagem,status)
+values
+	('bolsa dia a dia preta','único',130.00,'bolsas','assets/produtos/bolsa-1.jpg',1),
+	('bolsa rosa choque','único',140.00,'bolsas','assets/produtos/bolsa-2.jpg',	1),
 
 -- Ativando os produtos
 update produto set status=1 where id in(1,2,3,4,5,6,7,8,9,10,11,12);
@@ -78,14 +84,14 @@ values
 INSERT INTO
 	produto(descricao,tamanho,valor_unitario,categoria,imagem,status)
 values
-	('calÃ§a jogger preta','38',130.00,'calÃ§as','assets/produtos/calca-1.png',1),
-	('calÃ§a alfaiataria xadrez','36',140.00,'calÃ§as','assets/produtos/calca-2.png',1),
-	('calÃ§a jogger cinza','42',110.00,'calÃ§as','assets/produtos/calca-3.png',1);
+	('calça jogger preta','38',130.00,'calças','assets/produtos/calca-1.png',1),
+	('calça alfaiataria xadrez','36',140.00,'calças','assets/produtos/calca-2.png',1),
+	('calça jogger cinza','42',110.00,'calças','assets/produtos/calca-3.png',1);
 
 INSERT INTO
 	produto(descricao,tamanho,valor_unitario,categoria,imagem,status)
 values
-	('camisa listrada azul','G',1,130.00,'camisas','assets/produtos/camisa-1.png',1),
+	('camisa listrada azul','G',130.00,'camisas','assets/produtos/camisa-1.png',1),
 	('camisa listrada colorida','M',140.00,'camisas','assets/produtos/camisa-2.png',	1),
 	('camisa preta','PP',110.00,'camisas',	'assets/produtos/camisa-3.png',	1);
 
@@ -96,7 +102,7 @@ values
 	('mochila tie-dye now united',40,140.00,'mochilas','assets/produtos/mochila-2.png',1),
 	('mochila preta naruto',36,110.00,'mochilas','assets/produtos/mochila-3.png',1),
 	('mochila colorida hamster',36, 110.00,'mochilas','assets/produtos/mochila-4.png',1),
-	('mochila roxa unicÃ³rnio',36,110.00,'mochilas','assets/produtos/mochila-5.png',1);
+	('mochila roxa unicórnio',36,110.00,'mochilas','assets/produtos/mochila-5.png',1);
 
 INSERT INTO
 	produto(descricao,tamanho,valor_unitario,categoria,imagem,status)
@@ -105,6 +111,12 @@ values
 	('mocassim verde musgo',40,140.00,'sapatos','assets/produtos/sapato-2.png',1),
 	('sapato social marrom',36,110.00,'sapatos','assets/produtos/sapato-3.png',1),
 	('sapato boneca nude',36,110.00,'sapatos','assets/produtos/sapato-4.png',1);
+
+INSERT INTO
+	produto(descricao,tamanho,valor_unitario,categoria,imagem,status)
+values
+	('bolsa dia a dia preta','único',130.00,'bolsas','assets/produtos/bolsa-1.jpg',1),
+	('bolsa rosa choque','único',140.00,'bolsas','assets/produtos/bolsa-2.jpg',	1),
 
 -- PEDIDOS
 INSERT INTO
